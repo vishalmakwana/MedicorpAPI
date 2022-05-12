fa77a88ebd61d249d6f247443f675344fb8ad69f
Vishal <vishal.makwana@powertags.com>
2022-05-12 09:33:40 +0530

    CategoryMaster Interface Missing

---
+++ MedicorpWeb/Medicorp.IServices/ICategoryMasterService.cs
@@ -0,0 +1,21 @@
+﻿using Medicorp.Core;
+using Medicorp.Core.Entity.Master;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medicorp.IServices
+{
+    public interface ICategoryMasterService
+    {
+        Task<ApiResponse<List<CategoryMaster>>> GetCategoryAsync(CategoryMasterFilter filter);
+        Task<ApiResponse<int>> UpdateAsync(CategoryMaster categoryMaster);
+        Task<ApiResponse<int>> CreateAsync(CategoryMaster categoryMaster);
+        Task<ApiResponse<int>> DeleteAsync(int categoryId);
+        Task<ApiResponse<List<T>>> GetCategoryAsync<T>(CategoryMasterFilter filter) where T : class;
+        Task<bool> ValidateNameAsync(string CategoryName);
+        Task<bool> ValidateAsync(int CategoryId, string CategoryName);
+    }
+}
